210a6cc8035686b4e257d79e2e5b273eae5897f2
mano <abc@gmail.com>
2021-02-10 12:01:00 +0530

    first commit

---
+++ contact.py
@@ -0,0 +1,12 @@
+
+num = 7
+
+# To take input from the user
+#num = int(input("Enter a number: "))
+
+factorial = 1
+
+# check if the number is negative, positive or zero
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+
